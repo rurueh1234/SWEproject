3d29766388bac8addfc9004974b660923acb1bdd
rurueh1234 <ruba.emadd@gmail.com>
2025-03-26 01:24:55 +0300

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
-session_unset(); // Clear all session variables
-session_destroy(); // Destroy the session
+session_unset(); 
+session_destroy();
 
-// Redirect to login page
+
 header("Location: login.php");
 exit();
 ?>
