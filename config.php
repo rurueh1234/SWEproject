600d929a109dba413970ef3711b1582a223d7679
rurueh1234 <ruba.emadd@gmail.com>
2025-04-04 00:33:46 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,17 +1,12 @@
 <?php
-define('SITE_NAME', 'MetroX');
-define('BASE_URL', 'http://localhost/metrox/');
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "metrox";
 
-$host = 'localhost';
-$dbname = 'metrox';
-$username = 'root';
-$password = 'root';
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-} catch (PDOException $e) {
-    die("Database connection failed: " . $e->getMessage());
+if ($conn->connect_error) {
+    die("Database connection failed: " . $conn->connect_error);
 }
 ?>
