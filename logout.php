56c579171ac6299a3ec02f740a1c35bd17ba9f9f
Jana18kh <139820709+Jana18kh@users.noreply.github.com>
2025-04-10 07:01:04 +0300

    Delete logout.php

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-session_unset(); // Unset all session variables
-session_destroy(); // Destroy the session
-
-// Redirect to login page
-header("Location: login.html");
-exit();
-?>
