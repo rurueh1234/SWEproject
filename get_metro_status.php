9a0d24ba023754ee78d9705ceadf8c4f8c608cb5
remugh <renadalmughamis@gmail.com>
2025-04-20 19:20:09 +0300

    Update get_metro_status.php

--- get_metro_status.php
+++ get_metro_status.php
@@ -1,9 +1,19 @@
-
 <?php
 header('Content-Type: application/json');
 require_once 'config.php';
 
-$sql = "SELECT name, metroStatus FROM station";
+$sql = "
+SELECT 
+    s.name, 
+    s.metroStatus, 
+    (
+        SELECT MIN(a.arrivalTime)
+        FROM arrival_times a
+        WHERE a.stationID = s.stationID AND a.arrivalTime > CURTIME()
+    ) AS nextArrival
+FROM station s
+";
+
 $result = $conn->query($sql);
 
 $stations = [];
